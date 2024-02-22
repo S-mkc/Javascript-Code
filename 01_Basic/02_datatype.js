eae538bd4871f5ff54cb4309f003d0336c0d44ee
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-02-22 16:48:02 +0000

    dtatype node

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,14 @@
+/*
+number,
+bigint,
+string,
+boolean,
+null ->standalone value (empty value) (it is object)
+undefined -> declare but not assigned value (it is type )
+symbol -> to find uniqueness 
+
+*/
+
+//object 
+let name = "Mukesh"
+console.log(typeof "Mukesh");
